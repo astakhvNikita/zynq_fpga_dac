7e7f9c33f2c454596bbfe01a7f58584eb54c60ec
Никита Астахов <astakhv.nikita@gmail.com>
2025-03-16 20:44:41 +0300

    Пересборка проекта

--- hdl/ip/zynq_fpga_dac_top_design/hdl/zynq_fpga_dac_top_design_wrapper.v
+++ hdl/ip/zynq_fpga_dac_top_design/hdl/zynq_fpga_dac_top_design_wrapper.v
@@ -1,8 +1,8 @@
 //Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
-//Date        : Tue Aug 20 22:06:11 2024
-//Host        : DESKTOP-GTQVNVP running 64-bit major release  (build 9200)
+//Date        : Sun Mar 16 20:31:49 2025
+//Host        : DESKTOP-3ECOV03 running 64-bit major release  (build 9200)
 //Command     : generate_target zynq_fpga_dac_top_design_wrapper.bd
 //Design      : zynq_fpga_dac_top_design_wrapper
 //Purpose     : IP block netlist
